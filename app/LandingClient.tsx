76ca315d0ac4b2097d4c08956f81489d3b6ad794
KING6CODE <alistpassecoutrin@gmail.com>
2026-06-21 03:45:17 +0200

    Update LandingClient.tsx

--- app/LandingClient.tsx
+++ app/LandingClient.tsx
@@ -486,7 +486,7 @@ const GLOBAL_CSS = `
     border-radius: 20px;
     overflow: hidden;
     border: 1px solid rgba(124,58,237,0.3);
-    box-shadow: 0 20px 60px rgba(124,58,237,0.18), inset 0 1px 0 rgba(255,255,255,0.05);
+    box-shadow: 0 20px 60px rgba(115, 40, 46,0.18), inset 0 1px 0 rgba(255,255,255,0.05);
   }
   .avatar-photo-frame img {
     width: 100%;
